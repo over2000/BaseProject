97a35990452eff3e7dd2b8a02e86971b6703f134
over2000 <danielvitorcg@gmail.com>
2023-02-14 11:17:46 -0300

    remove profile on nav section

--- frontend/src/layouts/dashboard/nav/index.js
+++ frontend/src/layouts/dashboard/nav/index.js
@@ -57,24 +57,6 @@ export default function Nav({ openNav, onCloseNav }) {
         <Logo />
       </Box>
 
-      <Box sx={{ mb: 5, mx: 2.5 }}>
-        <Link underline="none">
-          <StyledAccount>
-            <Avatar src={account.photoURL} alt="photoURL" />
-
-            <Box sx={{ ml: 2 }}>
-              <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                {account.displayName}
-              </Typography>
-
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                {account.role}
-              </Typography>
-            </Box>
-          </StyledAccount>
-        </Link>
-      </Box>
-
       <NavSection data={navConfig} />
 
       <Box sx={{ flexGrow: 1 }} />
