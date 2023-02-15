712556ebac14419bd4645857aa88e0a705276e52
over2000 <danielvitorcg@gmail.com>
2023-02-15 11:32:37 -0300

    basic user model

---
+++ backend/app/Models/User.ts
@@ -0,0 +1,18 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel{
+  public static table = 'base.tb_user'
+
+  @column({ isPrimary: true })
+  public id_user: Number
+
+  @column()
+  public in_ativo: Boolean
+
+  @column()
+  public name: String
+
+  @column()
+  public function: String
+
+}
