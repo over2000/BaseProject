72ba8a0502eec7bd1da1e299bf64ebe6633390e6
over2000 <danielvitorcg@gmail.com>
2023-02-17 14:59:21 -0300

    snackbar provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,18 @@ import ThemeProvider from './theme';
 // components
 import ScrollToTop from './components/scroll-to-top';
 import { StyledChart } from './components/chart';
+import { SnackbarProvider } from 'notistack'
 
 // ----------------------------------------------------------------------
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <ScrollToTop />
-      <StyledChart />
-      <Router />
-    </ThemeProvider>
+    <SnackbarProvider maxSnack={5}>
+      <ThemeProvider>
+        <ScrollToTop />
+        <StyledChart />
+        <Router />
+      </ThemeProvider>
+    </SnackbarProvider>
   );
 }
