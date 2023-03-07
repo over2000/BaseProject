ea1d9ee6dc536fa527ba7eeea258dcd79a1c4c28
over2000 <danielvitorcg@gmail.com>
2023-03-07 16:58:38 -0300

    wip: create user form component

---
+++ frontend/src/components/create-user-form/CreateUserForm.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Dialog from '@mui/material/Dialog';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function CreateUserForm(isDialogOpened, handleCloseDialog) {
+
+
+  useEffect(() => {
+    handleClickOpen();
+  }, [])
+
+  const handleClose = () => {
+    handleCloseDialog(false);
+  }
+
+  const handleClickOpen = () => { }
+
+  return (
+    <div>
+      <Dialog
+        open={isDialogOpened}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Dialog>
+    </div>
+  );
+}
