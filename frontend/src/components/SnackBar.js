9584811d04de656857a0fc5949a7c8340665842a
over2000 <danielvitorcg@gmail.com>
2023-03-07 16:58:12 -0300

    wip: configure snack bar colors

--- frontend/src/components/SnackBar.js
+++ frontend/src/components/SnackBar.js
@@ -1,7 +1,7 @@
 export function errorHandling(message, enqueueSnackbar) {
   enqueueSnackbar(message, {
     variant: 'error',
-    //autoHideDuration: 3000,
+    autoHideDuration: 3000,
     persist: false
   })
 }
